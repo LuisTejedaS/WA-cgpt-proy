d605a369f2b8c510577ff639058a09a26a13702b
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2023-12-05 17:25:13 -0600

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,31 +1,33 @@
-async function callChatAPI(newMessage){
+async function callChatAPI(newMessage) {
+
+    const openAIKey = process.env.OPENAI_KEY;
 
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${openAIKey}`,
-            },
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo",
-                messages:
-                    [{
-                        role: "user",
-                        content: `You are a Product manager,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${openAIKey}`,
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages:
+                [{
+                    role: "user",
+                    content: `You are a Product manager,
                    You have been doing this task for 20 years, 
                    Your task is to generate as many acceptance criteria statements as you can for the following application: ${newMessage}
                    `
-                    }],
-                max_tokens: 1000, // Adjust based on your needs
-            }),
-        });
-        const data = await response.json();
-        return data.choices[0].message.content;
+                }],
+            max_tokens: 1000, // Adjust based on your needs
+        }),
+    });
+    const data = await response.json();
+    console.log(data)
+    return data.choices[0].message.content;
 }
 
-async function mockCallChatAPI(newMessage){
-
-        return 'this is a mock ' + newMessage;
+async function mockCallChatAPI(newMessage) {
+    return 'this is a mock ' + newMessage;
 }
 
 
@@ -38,12 +40,12 @@ export default async function handler(req, res) {
     };
 
     try {
-        chatResponse = await mockCallChatAPI(newMessage)
+        chatResponse = await callChatAPI(newMessage)
     } catch (error) {
         console.log(error);
         res.status(500).send({});
     }
-    res.status(200).send({content: chatResponse});
+    res.status(200).send({ content: chatResponse });
 
 }
 
