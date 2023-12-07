7c5b45623475dab08a62fd66239dd68e34062d73
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2023-12-07 16:50:43 -0600

    Update message.test.js

--- test/message.test.js
+++ test/message.test.js
@@ -1,10 +1,19 @@
 const generateAIMessage = require('../lib/message');
 
-test('should generate no functional criteria and calculator', () => {
+test('should generate no functional criteria and calculator message', () => {
 
     let message = `You are a Product manager,
     You have been doing this task for 20 years, 
     Your task is to generate as many acceptance criteria statements as you can for the following application: calculator. 
     `
   expect(generateAIMessage('calculator', false)).toBe(message);
+});
+
+test('should generate functional criteria and calculator message', () => {
+
+    let message = `You are a Product manager,
+    You have been doing this task for 20 years, 
+    Your task is to generate as many acceptance criteria statements as you can for the following application: calculator. 
+    Please, also add non-functional acceptance criteria and separate the functional of the non-functional`
+  expect(generateAIMessage('calculator', true)).toBe(message);
 });
